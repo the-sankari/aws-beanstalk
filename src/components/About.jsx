0a63cbd934b0c39b062a80eb39d3975ba759de01
Kajol sutra dhar <kajol.sd777@gmail.com>
2024-09-12 01:49:46 +0300

    Updated the react app and build

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Team 7</h1>
+      <p>We are Team 7, consisting of three awesome developers learning Continuous Integration and AWS.</p>
+    </div>
+  );
+}
+
+export default About;
